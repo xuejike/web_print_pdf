5b729e2382a21c44a3369409e29efbf75d2a0041
xueji <xuejike2004@126.com>
2019-07-30 08:38:26 +0800

    添加项目文件。

---
+++ pdf24sdPrinter/HttpService.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using Spire.Pdf;
+
+
+namespace pdf24sdPrinter
+{
+    class HttpService
+    {
+        private HttpListener _listener;
+        private bool isRun = true;
+        public void start()
+        {
+            // TODO: 在此处添加代码以启动服务。
+            _listener = new HttpListener();
+            _listener.Prefixes.Add("http://127.0.0.1:19996/");
+            _listener.Start();
+            _listener.BeginGetContext(Result,null);
+            
+        }
+
+        public void stop()
+        {
+            _listener.Stop();
+        }
+
+
+
+        private void Result(IAsyncResult ar)
+        {
+            //当接收到请求后程序流会走到这里
+
+            //继续异步监听
+            _listener.BeginGetContext(Result, null);
+            var guid = Guid.NewGuid().ToString();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"接到新的请求:{guid},时间：{DateTime.Now.ToString()}");
+            //获得context对象
+            var context = _listener.EndGetContext(ar);
+            var request = context.Request;
+            var response = context.Response;
+            ////如果是js的ajax请求，还可以设置跨域的ip地址与参数
+            //context.Response.AppendHeader("Access-Control-Allow-Origin", "*");//后台跨域请求，通常设置为配置文件
+            //context.Response.AppendHeader("Access-Control-Allow-Headers", "ID,PW");//后台跨域参数设置，通常设置为配置文件
+            //context.Response.AppendHeader("Access-Control-Allow-Method", "post");//后台跨域请求设置，通常设置为配置文件
+            context.Response.ContentType = "text/plain;charset=UTF-8";//告诉客户端返回的ContentType类型为纯文本格式，编码为UTF-8
+            context.Response.AddHeader("Content-type", "text/plain");//添加响应头信息
+            context.Response.ContentEncoding = Encoding.UTF8;
+            string returnObj = null;//定义返回客户端的信息
+            if (request.HttpMethod == "POST" && request.InputStream != null)
+            {
+                //处理客户端发送的请求并返回处理信息
+                returnObj = HandleRequest(request, response);
+            }
+            else
+            {
+                returnObj = $"不是post请求或者传过来的数据为空";
+            }
+            var returnByteArr = Encoding.UTF8.GetBytes(returnObj);//设置客户端返回信息的编码
+            try
+            {
+                using (var stream = response.OutputStream)
+                {
+                    //把处理信息返回到客户端
+                    stream.Write(returnByteArr, 0, returnByteArr.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"网络蹦了：{ex.ToString()}");
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"请求处理完成：{guid},时间：{ DateTime.Now.ToString()}\r\n");
+        }
+        private  string HandleRequest(HttpListenerRequest request, HttpListenerResponse response)
+        {
+            string data = null;
+            try
+            {
+                var byteList = new List<byte>();
+                var byteArr = new byte[2048];
+                int readLen = 0;
+                int len = 0;
+                //接收客户端传过来的数据并转成字符串类型
+                do
+                {
+                    readLen = request.InputStream.Read(byteArr, 0, byteArr.Length);
+                    len += readLen;
+                    byteList.AddRange(byteArr);
+                } while (readLen != 0);
+                data = Encoding.UTF8.GetString(byteList.ToArray(), 0, len);
+                download(data);
+             
+                //获取得到数据data可以进行其他操作
+            }
+            catch (Exception ex)
+            {
+                response.StatusDescription = "404";
+                response.StatusCode = 404;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"在接收数据时发生错误:{ex.ToString()}");
+                return ex.ToString();//把服务端错误信息直接返回可能会导致信息不安全，此处仅供参考
+            }
+            response.StatusDescription = "200";//获取或设置返回给客户端的 HTTP 状态代码的文本说明。
+            response.StatusCode = 200;// 获取或设置返回给客户端的 HTTP 状态代码。
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"接收数据完成:{data.Trim()},时间：{DateTime.Now.ToString()}");
+            return "打印成功"; ;
+        }
+
+        public void download(String url)
+        {
+            string tempFile = Path.GetTempFileName();
+
+            DownloadFile(url,tempFile);
+            var pdfDocument = new PdfDocument(tempFile);
+            pdfDocument.PrintSettings.PrintController = new StandardPrintController();
+            pdfDocument.Print();
+            Console.WriteLine("打印完成");
+        }
+
+
+        ///<summary>
+        /// 下载文件
+        /// </summary>
+        /// <param name="URL">下载文件地址</param>
+        /// <param name="Filename">下载后另存为（全路径）</param>
+        private bool DownloadFile(string URL, string filename)
+        {
+            try
+            {
+                HttpWebRequest Myrq = (System.Net.HttpWebRequest)System.Net.HttpWebRequest.Create(URL);
+                HttpWebResponse myrp = (System.Net.HttpWebResponse)Myrq.GetResponse();
+                Stream st = myrp.GetResponseStream();
+                Stream so = new System.IO.FileStream(filename, System.IO.FileMode.Create);
+                byte[] by = new byte[1024];
+                int osize = st.Read(by, 0, (int)by.Length);
+                while (osize > 0)
+                {
+                    so.Write(by, 0, osize);
+                    osize = st.Read(by, 0, (int)by.Length);
+                }
+                so.Close();
+                st.Close();
+                myrp.Close();
+                Myrq.Abort();
+                return true;
+            }
+            catch (System.Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ pdf24sdPrinter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.Text;
+
+namespace pdf24sdPrinter
+{
+
+ 
+    class Program
+    {
+        [DllImport("user32.dll", EntryPoint = "ShowWindow", SetLastError = true)]
+        static extern bool ShowWindow(IntPtr hWnd, uint nCmdShow);
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        static void Main(string[] args)
+        {
+            // var pdfDocument = new PdfDocument(@"C:\Users\xueji\Desktop\2019-6-24未命名文件.pdf");
+            //  pdfDocument.PrintSettings.PrintController = new StandardPrintController();
+
+            //pdfDocument.Print();
+            new HttpService().start();
+
+
+            if (args.Length > 0 && args[0] =="hide")
+            {
+                Console.Title = "24udprinter";
+                IntPtr intptr = FindWindow("ConsoleWindowClass", "24udprinter");
+                if (intptr != IntPtr.Zero)
+                {
+                    ShowWindow(intptr, 0);//隐藏这个窗口
+                }
+              
+            }
+            Console.ReadKey();
+       
+        }
+    }
+}
